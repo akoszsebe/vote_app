80f568a2dd55f44a75d20303130317fcc1bf364e
zsebe.akos <zsebe.akos@accenture.com>
2019-06-07 17:30:28 +0300

    app bar test

--- lib/notification/notificationscreen_view.dart
+++ lib/notification/notificationscreen_view.dart
@@ -18,12 +18,22 @@ class NotificationScreenState extends State<NotificationScreen> {
   List<NotificationResponse> notifications;
   NotificationScreenController _notificationScreenController;
 
+  var scrollController = ScrollController();
+
+  double scroll = 0.0;
+
   @override
   void initState() {
     super.initState();
     _notificationScreenController =
         NotificationScreenController(notificationScreenState: this);
     _notificationScreenController.init();
+    scrollController.addListener(() {
+      setState(() {
+          scroll = scrollController.offset/1.5;
+      });
+      print(scroll);
+    });
   }
 
   void addNotifications(List<NotificationResponse> notifications) {
@@ -38,13 +48,41 @@ class NotificationScreenState extends State<NotificationScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Notifications"),
-        ),
-        body: DarkRefreshIndicator(
-            key: _refreshIndicatorKey,
-            onRefresh: _notificationScreenController.refresh,
-            child: _buildBody(notifications)));
+        // appBar: AppBar(
+        //   title: Text("Notifications"),
+        // ),
+        // body: DarkRefreshIndicator(
+        //     key: _refreshIndicatorKey,
+        //     onRefresh: _notificationScreenController.refresh,
+        //     child: _buildBody(notifications)));
+        body: NestedScrollView(
+            controller: scrollController,
+            headerSliverBuilder:
+                (BuildContext context, bool innerBoxIsScrolled) {
+              return <Widget>[
+                SliverAppBar(
+                  elevation: 0,
+                  expandedHeight: 100.0,
+                  floating: false,
+                  pinned: true,
+                  flexibleSpace: FlexibleSpaceBar(
+                      centerTitle: true,
+                      title: Text("Notifications",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18.0,
+                          )),
+                      background:
+                          Container(color: Theme.of(context).primaryColor)),
+                ),
+              ];
+            },
+            body: Container(
+                padding: EdgeInsets.only(top: scroll),
+                child: DarkRefreshIndicator(
+                    key: _refreshIndicatorKey,
+                    onRefresh: _notificationScreenController.refresh,
+                    child: _buildBody(notifications)))));
   }
 
   Widget _buildBody(List<NotificationResponse> notifications) {
@@ -55,10 +93,13 @@ class NotificationScreenState extends State<NotificationScreen> {
 
   Widget _buildNotifications() {
     return Container(
+        decoration: new BoxDecoration(
+            color: Theme.of(context).primaryColorLight,
+            borderRadius: new BorderRadius.all(const Radius.circular(20.0))),
         alignment: Alignment.centerLeft,
         child: ListView.separated(
           separatorBuilder: (context, index) => Divider(
-                color: Theme.of(context).primaryColorLight,
+                color: Theme.of(context).primaryColor,
               ),
           itemBuilder: (context, index) {
             return NotificationListItem(
@@ -100,4 +141,3 @@ class NotificationScreenState extends State<NotificationScreen> {
         ]));
   }
 }
-
